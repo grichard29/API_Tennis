028e76a6403b317afd177ddd38cdfe88f374b972
grichard <guillaume.richard@keyconsulting.fr>
2022-10-11 09:46:37 +0200

    updated routes of the api

--- src/routes/players.route.js
+++ src/routes/players.route.js
@@ -3,15 +3,6 @@ const router = express.Router({ mergeParams: true });
 
 const playersController = require('../controllers/players.controller');
 
-router.route('/best-country')
-    .get(playersController.getBestCountry);
-
-router.route('/imc')
-    .get(playersController.getIMC);
-
-router.route('/median')
-    .get(playersController.getMedian);
-
 router.route('/')
     .get(playersController.getAll);
 
